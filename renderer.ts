479083faa66a027e38875029bc0b3040778ef85e
gness1804 <graham.nessler@gmail.com>
2022-01-11 20:29:28 -0600

    restyle button positioning and add line with file name

--- renderer.ts
+++ renderer.ts
@@ -4,9 +4,13 @@ fileInput.addEventListener('change', async (event: Event) => {
   const file = (event.target as HTMLInputElement).files[0];
   const filePath = await (window as any).transform.change(file);
   if (filePath) {
+    // create and activate download link
     const link = document.createElement('a');
     link.download = file.name.replace(/.HEIC$/i, '.jpg')
     link.href = filePath;
     link.click();
+    // add file name to app display
+    const { name: fileName } = file;
+    document.querySelector('.file-name').textContent = `Your file: ${fileName}`;
   }
 })
